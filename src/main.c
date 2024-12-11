6ab8163bd14350c6122e8365da6a8e70c278efb5
Taiga <combiasso57@gmail.com>
2024-12-11 16:07:54 +0100

    bouncing ball (:

--- src/main.c
+++ src/main.c
@@ -4,8 +4,10 @@
 #include "../include/engine.h"
 
 int main() {
+  
     init_engine();
-
+    int ball_x = 5, ball_y = 5;
+    int velocity_x = 1, velocity_y = 1;
     bool running = true;
     const int target_fps = 60;
     const double frame_time = 1.0 / target_fps;
@@ -22,14 +24,15 @@ int main() {
 
             // Clear the framebuffer
             clear_framebuffer();
+            // Move the ball
+            ball_x += velocity_x;
+            ball_y += velocity_y;
 
-            // Draw a simple object (e.g., a static rectangle)
-            for (int y = 5; y < 10; y++) {
-                for (int x = 10; x < 30; x++) {
-                    draw_pixel(x, y, '#');
-                }
-            }
-
+            // Bounce on edges
+            if (ball_x <= 0 || ball_x >= SCREEN_WIDTH - 1) velocity_x = -velocity_x;
+            if (ball_y <= 0 || ball_y >= SCREEN_HEIGHT - 1) velocity_y = -velocity_y;
+            // Draw the ball
+            draw_pixel(ball_x, ball_y, 'O');
             // Render the frame
             render_frame();
         }
@@ -38,3 +41,6 @@ int main() {
     shutdown_engine();
     return 0;
 }
+
+
+
